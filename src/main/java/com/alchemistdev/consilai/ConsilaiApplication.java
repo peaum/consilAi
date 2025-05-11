964e24baa1853fc0b7330d36d0c3b37bd85b2be1
Fran Gomez <gomezbalastegui96@gmail.com>
2025-05-11 19:38:08 +0200

    initial commit

---
+++ src/main/java/com/alchemistdev/consilai/ConsilaiApplication.java
@@ -0,0 +1,13 @@
+package com.alchemistdev.consilai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsilaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsilaiApplication.class, args);
+	}
+
+}
